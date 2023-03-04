d4f6f4cd028eb24288a93adb8f768157d8c8447c
HarounTheGreat <harounbouriga99@gmai.com>
2023-03-04 17:35:09 +0100

    Analysis-Social-Media-React-Project

--- src/pages/analyse_by_language.js
+++ src/pages/analyse_by_language.js
@@ -5,6 +5,7 @@ import {
 } from "./filtring_function";
 import React from "react";
 import Pie_chart from "./pie_chart";
+import "./chart.css";
 const Analyse_by_language = ({ trump_data }) => {
   let Languages_used_array;
   let result;
@@ -13,16 +14,24 @@ const Analyse_by_language = ({ trump_data }) => {
   console.log("result \n", result[0][0]);
   return (
     <div>
-      <h1> Analysis By Language</h1>
-      <div className="analyse_by_language">
+      {/* <h1> Analysis By Language</h1>
+      <div>
         {result.map((one_table) => {
           return (
-            <div>
+            <div className="analyse_by_language">
               <h1>{one_table[0].Language}</h1>
               <Pie_chart key={one_table.N} trump_data={one_table}></Pie_chart>
             </div>
           );
         })}
+      </div> */}
+      <div>
+        <div className="analyse_by_language">
+          <h1> Engish </h1>
+          <Pie_chart trump_data={result[0]}></Pie_chart>
+          <h1> Italian </h1>
+          <Pie_chart trump_data={result[1]}></Pie_chart>
+        </div>
       </div>
     </div>
   );
