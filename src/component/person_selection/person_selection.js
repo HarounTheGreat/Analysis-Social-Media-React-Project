1eb6ce96fb25d21a816f9243e698d78ea8f6a6e8
HarounTheGreat <harounbouriga99@gmai.com>
2023-03-21 19:34:11 +0100

    Analysis-Social-Media-React-Project

--- src/component/person_selection/person_selection.js
+++ src/component/person_selection/person_selection.js
@@ -43,10 +43,10 @@ const PersonSelection = ({
             </div>
           </div>
           {/* <div className="persons-names">
-            <div className="first-name">{person1_name}</div>
-            <div className="vs1">Vs</div>
-            <div className="second-name">{person2_name}</div>
-          </div> */}
+              <div className="first-name">{person1_name}</div>
+              <div className="vs1">Vs</div>
+              <div className="second-name">{person2_name}</div>
+            </div> */}
         </>
       )}
       <div className="year_selection">
@@ -67,7 +67,63 @@ const PersonSelection = ({
           />
         </div>
       </div>
-      {/* ---------------------------------------------------- */}
+      <div className="person-card">
+        <div className="person-card-img">
+          <img alt={person1_name} src={img1} />
+        </div>
+        <div className="person-card-desc">
+          <div className="person-card-fullname">Donald Trump</div>
+          <div className="person-card-title">Ex President of the USA</div>
+          <div className="person-card-description">
+            Donald John Trump is an American politician, media personality, and
+            businessman who served as the 45th president of the United States
+            from 2017 to 2021.
+          </div>
+        </div>
+      </div>
+      {/* <div className="scroll-bg">
+        <img alt={person2_name} src={img2} />
+        <div className="scroll-div">
+          <div className="scroll-object">
+            simple scroll bar in div tutorial using only html and css if you
+            have questions concerning this video feel free to head down to the
+            comment section - - - - - - - - - - - -- - - - - - - - - - - - - - -
+            - - - - - - - - - - - - - - - - - - if you find this video helpful
+            please like and susbcribe
+            --------------------------------------------------------------------------------------
+            Track: Netrum - Colorblind (feat. Halvorsen) [NCS Release] Music
+            provided by NoCopyrightSounds.simple scroll bar in div tutorial
+            using only html and css if you have questions concerning this video
+            feel free to head down to the comment section - - - - - - - - - - -
+            -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            if you find this video helpful please like and susbcribe
+            --------------------------------------------------------------------------------------
+            Track: Netrum - Colorblind (feat. Halvorsen) [NCS Release] Music
+            provided by NoCopyrightSounds.simple scroll bar in div tutorial
+            using only html and css if you have questions concerning this video
+            feel free to head down to the comment section - - - - - - - - - - -
+            -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            if you find this video helpful please like and susbcribe
+            --------------------------------------------------------------------------------------
+            Track: Netrum - Colorblind (feat. Halvorsen) [NCS Release] Music
+            provided by NoCopyrightSounds.mple scroll bar in div tutorial using
+            only html and css if you have questions concerning this video feel
+            free to head down to the comment section - - - - - - - - - - - -- -
+            - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - if you
+            find this video helpful please like and susbcribe
+            --------------------------------------------------------------------------------------
+            Track: Netrum - Colorblind (feat. Halvorsen) [NCS Release] Music
+            provided by NoCopyrightSounds.simple scroll bar in div tutorial
+            using only html and css if you have questions concerning this video
+            feel free to head down to the comment section - - - - - - - - - - -
+            -- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+            if you find this video helpful please like and susbcribe
+            --------------------------------------------------------------------------------------
+            Track: Netrum - Colorblind (feat. Halvorsen) [NCS Release] Music
+            provided by NoCopyrightSounds.
+          </div>
+        </div>
+      </div> */}
     </div>
   );
 };
